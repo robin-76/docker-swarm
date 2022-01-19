54b7ef7e12a1c1251a37a427e273b61d8e9ef995
Maxence Bourgeaux <maxence.bourgeaux@etu.univ-lehavre.fr>
2022-01-19 16:05:29 +0100

    Ajout du temps en secondes

--- database/models/hash.js
+++ database/models/hash.js
@@ -1,7 +1,7 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-var Hash = new Schema({
+const Hash = new Schema({
   hash: {
     type: String,
     required: true,
